26c33fd6e337a0231d982ffcecf840562e077857
darksidefriend <dansavels@yandex.ru>
2026-03-05 00:59:58 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+DB_PATH = BASE_DIR / "terms.db"
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 # URL базы данных SQLite (файл terms.db будет создан в папке backend)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data/terms.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./data/terms.db"
 
 # Движок SQLAlchemy. Для SQLite нужно передать connect_args, чтобы разрешить использование в нескольких потоках
 engine = create_engine(
